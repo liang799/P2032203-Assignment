42f50e1ec0a757ab92e0fa6e1eb428cbaa7750be
liang799 <71926348+liang799@users.noreply.github.com>
2021-12-14 16:07:48 +0800

    Backed up files

---
+++ app/src/main/java/com/sp/p2032203assignment/Splash.java
@@ -0,0 +1,31 @@
+package com.sp.p2032203assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();   // hide title bar
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splash.this, Home.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+
+    }
+}
