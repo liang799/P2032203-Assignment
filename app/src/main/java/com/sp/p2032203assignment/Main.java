9d609c98d432eab96a76772268197b4c0c9e2956
liang799 <neohtp@tutanota.com>
2021-12-16 15:24:29 +0800

    Changed Action Bar to ToolBar

--- app/src/main/java/com/sp/p2032203assignment/Main.java
+++ app/src/main/java/com/sp/p2032203assignment/Main.java
@@ -1,11 +1,12 @@
 package com.sp.p2032203assignment;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
-
-import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,9 +16,12 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,  R.id.fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
