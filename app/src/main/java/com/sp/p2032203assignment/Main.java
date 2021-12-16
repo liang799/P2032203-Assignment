11da3ea8fad2c44b99e97680a581cd3cf88ffbc3
liang799 <71926348+liang799@users.noreply.github.com>
2021-12-16 12:30:10 +0800

    Bottom NavBar Controller working

--- app/src/main/java/com/sp/p2032203assignment/Main.java
+++ app/src/main/java/com/sp/p2032203assignment/Main.java
@@ -1,8 +1,13 @@
 package com.sp.p2032203assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Main extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
